f0db355300b223b0d0cd1e980eb49ac6bf82457d
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2024-01-31 23:07:20 -0300

    routesCorrigidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,8 @@ import { MovieDetailsComponent } from './pages/movie-details/movie-details.compo
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: '/search', component:SearchComponent},
-  {path: '/movie/:id', component:MovieDetailsComponent}
+  {path: './pages/search', component:SearchComponent},
+  {path: './pages/movie/:id', component:MovieDetailsComponent}
 ];
 
 @NgModule({
