d60e170c3c3d060e342309131117a8464a8e9cba
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2024-01-31 23:06:21 -0300

    rotas_routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: '/search', component:SearchComponent},
+  {path: '/movie/:id', component:MovieDetailsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
