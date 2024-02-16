84ed3bdc361656a109f07f719224368ef881f261
Luana Albuquerque Moreira <luanapaixao.am@gmail.com>
2024-02-16 13:08:31 -0300

    alteracoes-finais-tipos-filmes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,10 +16,24 @@ export class HomeComponent{
 
   bannerResult:any = [];
   trendingMovieResult: any[] = [];
+  actionMovieResult: any = [];
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+  documentaryMovieResult: any = [];
+  sciencefictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
 
   ngOnInit(): void {
     this.bannerData();
     this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.comedyMovie();
+    this.animationMovie();
+    this.documentaryMovie();
+    this.sciencefictionMovie();
+    this.thrillerMovie();
   }
 
   bannerData(){
@@ -33,5 +47,48 @@ export class HomeComponent{
       this.trendingMovieResult = result.results;
     });
   }
+
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieResult = result.results;
+    });
+  }
+
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieResult = result.results;
+    });
+  }
+
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieResult = result.results;
+    });
+  }
+
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieResult = result.results;
+    });
+  }
+
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      this.documentaryMovieResult = result.results;
+    });
+  }
+
+  sciencefictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      this.sciencefictionMovieResult = result.results;
+    });
+  }
+
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResult = result.results;
+    });
+  }
+
 
 }
